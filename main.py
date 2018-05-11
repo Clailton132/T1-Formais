55bb0569beda67d659c781369fd03d75d7926b38
gablanger <gabriel.langer94@gmail.com>
2018-05-11 01:48:15 -0300

    Add save/load regular grammar feature

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from models import RG
+#!/usr/bin/env python # -*- coding: utf-8 -*
 from tkinter import *
-import pprint
 import tkinter.messagebox
+from models import RG
+import os
+import pickle
 
-g = RG()
+if os.path.isfile("reg_grammar.p"):
+    all_reg_grammars = pickle.load( open( "reg_grammar.p", "rb" ) )
+    print all_reg_grammars
+else:
+    all_reg_grammars = {}
+    #print "File does not exist"
 
-def gui_addRule():
-    a = newEntry.get()
-    b = newEntry2.get()
-    g.add_rule(a,b)
+
+def get_formatted_production(g):
     lines = "G: P = {\n"
     for production in g.G.keys():
         rules = ""
         for rule in g.G[production]:
             rules += str(rule) + " | "
         rules = rules[0:-3]
-        lines += (str(production) + "-> " + rules + "\n")
+        lines += (str(production) + " --> " + rules + "\n")
     lines += "}"
-    name = lines
+    return lines
+
+def gui_addRule():
+    a = newEntry.get()
+    b = newEntry2.get()
+    g.add_rule(a,b)
+    name = get_formatted_production(g)
     labelText.set(name)
     #newEntry.delete(0, END)
     newEntry2.delete(0, END)
@@ -33,48 +44,84 @@ def gui_checkInput():
             text = "Your input is accepted by the grammar G"
     labelCheckInput.set(text)
 
+def gui_saveGrammar():
+    filename = filenameEntry.get()
+    all_reg_grammars[filename] = g.G
+    pickle.dump(all_reg_grammars, open( "reg_grammar.p", "wb" ))
 
-app = Tk()
-app.title("RegLangs")
-app.geometry('1200x700+200+200')
-
-labelError = StringVar()
-# labelError.set("SOME ERROR")
-label0 = Label(app, textvariable=labelError, height=1)
-label0.pack()
-Label(app, text="Regular Grammar Application", height=2, font=("Helvetica", 32)).pack()
-labelText = StringVar()
-labelText.set("G: P = ")
-label1 = Label(app, textvariable=labelText, height=6, font=("Helvetica", 16), borderwidth=2, relief="groove", padx=60,pady=15)
-label1.pack()
-
-Label(app, text="Production Left Side", height=2, font=("Helvetica", 16)).pack()
-A = StringVar(None)
-newEntry = Entry(app, textvariable=A)
-newEntry.insert(0,"S")
-newEntry.pack()
-Label(app, text="Production Right Side", height=2, font=("Helvetica", 16)).pack()
-B = StringVar(None)
-newEntry2 = Entry(app, textvariable=B)
-newEntry2.insert(0,"")
-newEntry2.pack()
-
-button1 = Button(app, text="Add rule", width=20, command=gui_addRule)
-button1.pack(side='top', padx=15,pady=15)
+def gui_loadGrammar():
+    filename = filenameEntry.get()
+    g.G = all_reg_grammars[filename]
+    name = get_formatted_production(g)
+    labelText.set(name)
 
 
-Label(app, text="Test input", height=2, font=("Helvetica", 24)).pack()
-#Label(app, text="Production Right Side", height=2, font=("Helvetica", 18)).pack()
-labelCheckInput = StringVar()
-label3 = Label(app, textvariable=labelCheckInput, height=2, font=("Helvetica", 14))
-label3.pack()
-Label(app, text="Input", height=2, font=("Helvetica", 12)).pack()
-input = StringVar(None)
-newEntry3 = Entry(app, textvariable=input)
-newEntry3.insert(0,"")
-newEntry3.pack()
-button2 = Button(app, text="TEST", width=20, command=gui_checkInput)
-button2.pack(side='top', padx=15,pady=15)
+
+while True:
+    os.system('clear')
+    print "**********************************"
+    print " Trabalho 1: Linguagens Regulares"
+    print "**********************************"
+    print "* [1] Criar Gramática Regular"
+    print "* [2] Listar Gramáticas Salvas"
+    print "* [3] Criar Expressão Regular"
+    print "* [9] Sair"
+    option = raw_input("\nOpção: ")
+    if option == "9":
+        break
+    if option == "2":
+        print all_reg_grammars
+        raw_input("\nAperte para continuar...")
+    if option == "1":
+        g = RG()
+        app = Tk()
+        app.title("RegLangs")
+        app.geometry('500x800+200+200')
+
+        labelError = StringVar()
+        # labelError.set("SOME ERROR")
+        label0 = Label(app, textvariable=labelError, height=1)
+        label0.pack()
+        Label(app, text="Regular Grammar Application", height=1, font=("Helvetica", 28)).pack()
+        labelText = StringVar()
+        labelText.set("G: P = ")
+        label1 = Label(app, textvariable=labelText, height=10, font=("Helvetica", 16), borderwidth=2, relief="groove", padx=60,pady=15)
+        label1.pack()
+
+        Label(app, text="Production Left Side", height=1, font=("Helvetica", 16)).pack()
+        A = StringVar(None)
+        newEntry = Entry(app, textvariable=A)
+        newEntry.insert(0,"S")
+        newEntry.pack()
+        Label(app, text="Production Right Side", height=1, font=("Helvetica", 16)).pack()
+        B = StringVar(None)
+        newEntry2 = Entry(app, textvariable=B)
+        newEntry2.insert(0,"")
+        newEntry2.pack()
+
+        button1 = Button(app, text="Add rule", width=20, command=gui_addRule)
+        button1.pack(side='top', padx=15,pady=15)
+
+        filename = StringVar(None)
+        filenameEntry = Entry(app, textvariable=filename)
+        filenameEntry.insert(0,"filename")
+        filenameEntry.pack()
+        buttonLoad = Button(app, text="Load Grammar", width=20, command=gui_loadGrammar)
+        buttonLoad.pack(side='top', padx=15,pady=5)
+        buttonSave = Button(app, text="Save Grammar", width=20, command=gui_saveGrammar)
+        buttonSave.pack(side='top', padx=15,pady=5)
 
 
-app.mainloop()
+        Label(app, text="Test input", height=1, font=("Helvetica", 24)).pack()
+        #Label(app, text="Production Right Side", height=2, font=("Helvetica", 18)).pack()
+        labelCheckInput = StringVar()
+        label3 = Label(app, textvariable=labelCheckInput, height=2, font=("Helvetica", 14))
+        label3.pack()
+        Label(app, text="Input", height=1, font=("Helvetica", 12)).pack()
+        input = StringVar(None)
+        newEntry3 = Entry(app, textvariable=input)
+        newEntry3.insert(0,"")
+        newEntry3.pack()
+        button2 = Button(app, text="TEST", width=20, command=gui_checkInput)
+        button2.pack(side='top', padx=15,pady=15)
+        app.mainloop()
